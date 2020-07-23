fd2773eb242c528e8bb8c289159f692b71dea398
Omar Aguirre <omers@octahedroid.com>
2020-07-22 19:01:46 -0500

    feat: adding revision to multidev (#16)
    * feat: adding revision to multidev
    
    * fix: deploy command
    
    * fix: adding dir to docker build
    
    * fix: set-tag revision to cloud run
    
    * fix: changing color
    
    * fix: changin tag name

--- frontity/packages/twentytwenty-theme/src/index.js
+++ frontity/packages/twentytwenty-theme/src/index.js
@@ -25,7 +25,7 @@ const twentyTwentyTheme = {
         primary: "#cd2653",
         headerBg: "#ffffff",
         footerBg: "#ffffff",
-        bodyBg: "#f5efe0"
+        bodyBg: "#62ccca"
       },
       // Whether to show the search button in page header
       showSearchInHeader: true,

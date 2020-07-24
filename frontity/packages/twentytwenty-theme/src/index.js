9a203f8b7ce141a17f92c933c8da8fef1b77cc29
Omar Aguirre <omers@octahedroid.com>
2020-07-24 15:37:05 -0500

    bg color

--- frontity/packages/twentytwenty-theme/src/index.js
+++ frontity/packages/twentytwenty-theme/src/index.js
@@ -25,7 +25,7 @@ const twentyTwentyTheme = {
         primary: "#cd2653",
         headerBg: "#ffffff",
         footerBg: "#ffffff",
-        bodyBg: "#ff6d29"
+        bodyBg: "#fffaad"
       },
       // Whether to show the search button in page header
       showSearchInHeader: true,

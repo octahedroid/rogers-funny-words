3f1ccddbedd7e449021bc9917105a55ee8a73b20
Omar Aguirre <omersguchigu@gmail.com>
2020-08-06 01:48:21 -0500

    feat: removing ci files and changing color

--- frontity/packages/twentytwenty-theme/src/index.js
+++ frontity/packages/twentytwenty-theme/src/index.js
@@ -25,7 +25,7 @@ const twentyTwentyTheme = {
         primary: "#cd2653",
         headerBg: "#ffffff",
         footerBg: "#ffffff",
-        bodyBg: "#ff6d29"
+        bodyBg: "#b18de3"
       },
       // Whether to show the search button in page header
       showSearchInHeader: true,
